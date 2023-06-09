7864f4d9e2cb72eb51b349d2d8fecb93d7b9f636
Linker-MSI <leochien@linkervision>
2023-06-09 17:07:12 +0800

    yaml.load to split input topics

--- ros_param_test/script/test_param_list.py
+++ ros_param_test/script/test_param_list.py
@@ -4,13 +4,18 @@ This script is used to test the parameter list
 """
 
 import rospy
+import yaml
 
 if __name__ == '__main__':
     # Initialize node
     rospy.init_node('test_param_list', anonymous=True)
 
     # Get parameters
-    
     input_topic = rospy.get_param("~input_topics", "/camera1/color/image_raw, /camera2/color/image_raw")
     print('input_topic:',input_topic)
 
+    # split the input_topic yaml format
+    topics = yaml.load(input_topic, Loader=yaml.FullLoader)
+
+    print('topic1:',topics[0])
+    print('topic2:',topics[1])
